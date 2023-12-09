47e83893db5939f22153f989482268f88ca0627b
ymei2296236 <e2296236@cmaisonneuve.qc.ca>
2023-12-09 16:48:17 -0500

    Corriger formulaire createTimbre, validation des champs

--- App/Controllers/Profil.php
+++ App/Controllers/Profil.php
@@ -40,23 +40,36 @@ class Profil extends \Core\Controller
     {
         \App\Library\CheckSession::sessionAuth(FALSE);
         extract($_POST);
-        
+
         // echo "<pre>";
         // print_r($_POST);
-        
+        // print_r($_FILES);
+
         $validation = new \App\Library\Validation;
- 
         $validation->name('Nom')->value($nom)->max(100)->min(2);
-        $validation->name('Description')->value($description)->max(100);
+        $validation->name('Description')->value($nom_2)->max(100);
         $validation->name('État')->value($etat_id)->required();
         $validation->name('Dimension')->value($dimension_id)->required();
         $validation->name('Pays')->value($pays_id)->required();
-        $validation->name('Extrait')->value($extrait)->max(225);
-        $validation->name('Image')->value($upload)->required();
-        // $validation->name('Tirage')->value($tirage)->pattern('int');
 
         $msg = '';
 
+        if (!empty($_FILES['img']['name'][0])) 
+        {
+            $img = $_FILES['img'];
+            $img_desc = \App\Library\UploadFiles::reArrayFiles($img);
+            
+            foreach($img_desc as $val)
+            {
+                $checkImg = getimagesize($val["tmp_name"]);
+                
+                if($checkImg == false)  
+                $msg = "Le fichier téléversé n'est pas une image.";
+            
+            // if($val["size"]> 120000)
+            // $msg = "Le fichier téléversé dépasse la taille maximale de 120ko.";
+            }
+        }
 
         $etat = new \App\Models\Etat;
         $etats = $etat->select('nom');
@@ -75,7 +88,7 @@ class Profil extends \Core\Controller
             else
                 $errors = '';
             
-            View::renderTemplate('Timbre/create.html', ['errors'=> $errors, 'etats'=>$etats, 'dimensions'=>$dimensions, 'tousPays'=>$tousPays, 'timbre'=>$_POST]);
+            View::renderTemplate('Timbre/create.html', ['errors'=> $errors, 'msg'=> $msg,'etats'=>$etats, 'dimensions'=>$dimensions, 'tousPays'=>$tousPays, 'timbre'=>$_POST]);
         } 
         else
         {
@@ -83,22 +96,25 @@ class Profil extends \Core\Controller
             $timbre = new \App\Models\Timbre;
             $insertTimbre = $timbre->insert($_POST);
 
-            // téléverse le fichier au dossier uploads
-            $img = $_FILES['img'];
-            $img_desc = \App\Library\UploadFiles::reArrayFiles($img);
-            $name = $_POST['nom'];
-            $folder = $_SERVER['DOCUMENT_ROOT'] . "/Stampee/uploads/";
-            $_POST['timbre_id'] = $insertTimbre; 
-            
-            foreach($img_desc as $val)
+            // téléverse le fichier au dossier uploads s'il y en a;
+            if (!empty($_FILES['img']['name'][0])) 
             {
-                $newname = $name."_".date('YmdHis',time())."_".mt_rand().".jpg";
-                move_uploaded_file($val['tmp_name'], $folder.$newname);
+                $img = $_FILES['img'];
+                $img_desc = \App\Library\UploadFiles::reArrayFiles($img);
+                $name = $_POST['nom'];
+                $folder = $_SERVER['DOCUMENT_ROOT'] . "/Stampee/uploads/";
+                $_POST['timbre_id'] = $insertTimbre; 
                 
-                $_POST['nom'] = $newname;
-                
-                $image = new \App\Models\Image;
-                $insertImage = $image->insert($_POST);
+                foreach($img_desc as $val)
+                {
+                    $newname = $name."_".date('YmdHis',time())."_".mt_rand().".jpg";
+                    move_uploaded_file($val['tmp_name'], $folder.$newname);
+                    
+                    $_POST['nom'] = $newname;
+                    
+                    $image = new \App\Models\Image;
+                    $insertImage = $image->insert($_POST);
+                }
             }
 
             View::renderTemplate('Profil/index.html');
--- App/Models/Timbre.php
+++ App/Models/Timbre.php
@@ -9,7 +9,7 @@ class Timbre extends CRUD
 {
     protected $table = 'timbre';
     protected $primaryKey = 'id';
-    protected $fillable = ['id', 'createur_id', 'nom', 'description', 'date_emission', 'couleur', 'tirage', 'extrait', 'certification', 'etat_id', 'dimension_id', 'pays_id'];
+    protected $fillable = ['id', 'createur_id', 'nom', 'nom_2', 'date_emission', 'couleur', 'tirage', 'extrait', 'certification', 'etat_id', 'dimension_id', 'pays_id'];
 }
 
 ?>
